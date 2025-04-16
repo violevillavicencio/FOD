ed2cf25eea7e36fc8c1d45969d7f6b176acec9f6
Violeta Villavicencio <violetapvillavicencio@gmail.com>
2025-04-16 00:37:14 -0300

    Update ejercicio09.pas

--- practica-2/ejercicio09.pas
+++ practica-2/ejercicio09.pas
@@ -1,3 +1,15 @@
+{
+Se cuenta con un archivo que posee información de las ventas que realiza una empresa a 
+los diferentes clientes. Se necesita obtener un reporte con las ventas organizadas por 
+cliente. Para ello, se deberá informar por pantalla: los datos personales del cliente, el total 
+mensual (mes por mes cuánto compró) y finalmente el monto total comprado en el año por el 
+cliente. Además, al finalizar el reporte, se debe informar el monto total de ventas obtenido 
+por la empresa.  
+El formato del archivo maestro está dado por: cliente (cod cliente, nombre y apellido), año, 
+mes, día y monto de la venta. El orden del archivo está dado por: cod cliente, año y mes. 
+Nota: tenga en cuenta que puede haber meses en los que los clientes no realizaron 
+compras. No es necesario que informe tales meses en el reporte.
+}
 program ejercicio9;
 const
     valorAlto = 9999;
