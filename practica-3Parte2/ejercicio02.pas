f9b2fc2c35125b36c67b78d684a8d8a4058f1541
Violeta Villavicencio <violetapvillavicencio@gmail.com>
2025-05-20 00:59:31 -0300

    Update ejercicio02.pas

--- practica-3Parte2/ejercicio02.pas
+++ practica-3Parte2/ejercicio02.pas
@@ -1,15 +1,57 @@
-program ejercicio2;
+{2. Se necesita contabilizar los votos de las diferentes mesas electorales registradas por
+localidad en la provincia de Buenos Aires. Para ello, se posee un archivo con la
+siguiente información: código de localidad, número de mesa y cantidad de votos en
+dicha mesa. Presentar en pantalla un listado como se muestra a continuación:
+Código de Localidad              Total de Votos
+................................ ......................
+................................ ......................
+Total General de Votos: ………………
+NOTAS:
+● La información en el archivo no está ordenada por ningún criterio.
+● Trate de resolver el problema sin modificar el contenido del archivo dado.
+● Puede utilizar una estructura auxiliar, como por ejemplo otro archivo, para
+llevar el control de las localidades que han sido procesadas.}
 
+program ejercicio2;
 type
     localidad = record
         codigo: integer;
         mesa: integer;
         cant: integer;
     end;
-
     archivo = file of localidad;
 
-{ Carga los datos desde archivo.txt al archivo binario original }
+procedure corteDeControl(var arc: archivo; var arcAux: archivo; var cantTotal: integer);
+var
+    l, aux: localidad;
+    ok: boolean;
+begin
+    reset(arc);
+    assign(arcAux, 'ArchivoAuxiliar');
+    rewrite(arcAux);   //estructura auxiliar para llevar el control de las localidades que han sido procesadas.
+    cantTotal:= 0;
+    while(not eof(arc)) do begin
+        read(arc, l);
+        ok:= false;
+        seek(arcAux, 0);
+        while(not eof(arcAux) and (not ok)) do begin
+            read(arcAux, aux);
+            if (aux.codigo = l.codigo) then
+                ok:= true;
+        end;
+        if(ok) then begin
+            aux.cant:= aux.cant + l.cant;
+            seek(arcAux, filepos(arcAux)-1);
+            write(arcAux, aux);
+        end
+        else
+            write(arcAux, l);
+            cantTotal:= cantTotal + l.cant;
+        end;
+    close(arc);
+    close(arcAux);
+end;
+
 procedure cargarArchivo(var arc: archivo);
 var
     txt: text;
@@ -17,89 +59,36 @@ var
 begin
     assign(txt, 'archivo.txt');
     reset(txt);
-
     assign(arc, 'ArchivoMaestro');
     rewrite(arc);
-
-    while not eof(txt) do
+    while(not eof(txt)) do
     begin
-        with l do
-        begin
-            readln(txt, codigo, mesa, cant);
-            write(arc, l);
+            with l do
+                begin
+                    readln(txt, codigo, mesa, cant);
+                    write(arc, l);
+                end;
         end;
-    end;
-
-    writeln('Archivo maestro generado.');
+    writeln('Archivo maestro generado');
+    close(arc);
     close(txt);
-    close(arc);
 end;
 
-{ Realiza el procesamiento sin modificar el archivo original }
-procedure corteDeControl(var arc: archivo; var arcAux: archivo; var cantTotal: integer);
-var
-    l, aux: localidad;
-    encontrado: boolean;
-begin
-    reset(arc);
-    assign(arcAux, 'ArchivoAuxiliar');
-    rewrite(arcAux);
-    cantTotal := 0;
-
-    while not eof(arc) do
-    begin
-        read(arc, l);
-        encontrado := false;
-        seek(arcAux, 0);
-
-        while (not eof(arcAux)) and (not encontrado) do
-        begin
-            read(arcAux, aux);
-            if aux.codigo = l.codigo then
-                encontrado := true;
-        end;
-
-        if encontrado then
-        begin
-            aux.cant := aux.cant + l.cant;
-            seek(arcAux, filepos(arcAux) - 1);
-            write(arcAux, aux);
-        end
-        else
-        begin
-            // Copiar solo código y votos (ignoramos el número de mesa para el auxiliar)
-            aux.codigo := l.codigo;
-            aux.cant := l.cant;
-            write(arcAux, aux);
-        end;
-
-        cantTotal := cantTotal + l.cant;
-    end;
-
-    close(arc);
-    close(arcAux);
-end;
-
-{ Imprime el archivo auxiliar con los totales por localidad }
 procedure imprimirArchivo(var arc: archivo; cantTotal: integer);
 var
     l: localidad;
 begin
     reset(arc);
-    writeln('Codigo de Localidad         Total de Votos');
-    writeln('---------------------       ----------------');
-
-    while not eof(arc) do
-    begin
-        read(arc, l);
-        writeln(l.codigo:10, '              ', l.cant);
-    end;
-
+    writeln('Codigo de Localidad          Total de Votos');
+    while(not eof(arc)) do
+        begin
+            read(arc, l);
+            writeln(l.codigo, '             ', l.cant);
+        end
     writeln('Total General de Votos: ', cantTotal);
     close(arc);
 end;
 
-{ Programa principal }
 var
     arc, arcAux: archivo;
     cantTotal: integer;
